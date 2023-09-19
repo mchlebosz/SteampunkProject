21bfe2068fddae2e749d9a8bf9832ccd3411b197
Mateusz Chlebosz <mateusz.chlebosz@outlook.com>
2023-09-19 22:15:42 +0200

    stop shining like dogs balls

--- f_simplest.glsl
+++ f_simplest.glsl
@@ -3,29 +3,38 @@
 uniform sampler2D textureMap0;
 uniform sampler2D textureMap1;
 
-out vec4 pixelColor; //Zmienna wyjsciowa fragment shadera. Zapisuje sie do niej ostateczny (prawie) kolor piksela
+out vec4 pixelColor;
 
 in vec2 iTexCoord0;
 in vec4 ic;
-in vec4 n;
-in vec4 l;
-in vec4 v;
 
 void main(void) {
+    // Retrieve the normal from textureMap1 (normal map)
+    vec3 normalMap = 2.0 * texture(textureMap1, iTexCoord0).xyz - 1.0;
 
-	//Znormalizowane interpolowane wektory
-	vec4 ml = normalize(l);
-	vec4 mn = normalize(n);
-	vec4 mv = normalize(v);
-	//Wektor odbity
-	vec4 mr = reflect(-ml, mn);
+    // Normalize the interpolated normal
+    vec3 normal = normalize(normalMap);
 
-	//Parametry powierzchni
-	vec4 kd = texture(textureMap0, iTexCoord0);
-	vec4 ks = texture(textureMap1, iTexCoord0);
+    // View direction
+    vec3 viewDir = normalize(-ic.xyz);
 
-	//Obliczenie modelu oświetlenia
-	float nl = clamp(dot(mn, ml), 0, 1);
-	float rv = pow(clamp(dot(mr, mv), 0, 1), 50);
-	pixelColor = vec4(kd.rgb * nl, kd.a) + vec4(ks.rgb * rv, 0);
+    // Light direction (assuming light is at a fixed position)
+    vec3 lightDir = normalize(vec3(1.0, 1.0, 1.0));
+
+    // Diffuse reflection (Lambertian)
+    float diffuse = max(dot(normal, lightDir), 0.0);
+
+    // Specular reflection (Blinn-Phong model)
+    vec3 halfwayDir = normalize(lightDir + viewDir);
+    float specular = pow(max(dot(normal, halfwayDir), 0.0), 50);
+
+    // Retrieve the diffuse and specular colors from textureMap0
+    vec4 kd = texture(textureMap0, iTexCoord0);
+    vec4 ks = vec4(1.0, 1.0, 1.0, 1.0); // Assuming a white specular color for simplicity
+
+    // Calculate the final pixel color
+    vec3 ambientColor = vec3(0.1, 0.1, 0.1); // Ambient lighting
+    vec3 finalColor = kd.rgb * diffuse + ks.rgb * specular + ambientColor;
+
+    pixelColor = vec4(finalColor, kd.a);
 }
